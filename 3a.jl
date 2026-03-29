da23864f64b26c866f5a82741742478f021dc70b
Nagarjuna N <acarjunarjun@gmail.com>
2026-03-30 05:26:01 +0530

    Implement interest calculation over 10 years

---
+++ 3a.jl
@@ -0,0 +1,16 @@
+println("enter the principle amount:")
+p = parse("float64", readline() )
+println("enter the rate of interest:")
+r = parse("float64", readline() )
+
+curent= p
+for year in 1:10
+    current <=currenrt * r / 100
+
+     println("year $year :\$ (round(p digits=2))")
+
+    if current <=current*2
+        println("the anount is exceded , stoping the calculation.")
+        break   
+    end
+end
